16873e868e5ef0bebc8579abfe5b186f98095f26
shyrochki <155745701+shyrochki@users.noreply.github.com>
2024-03-22 15:58:29 +0000

    Add files via upload

---
+++ milestone_7/flask_server.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+with open('database.json','r') as file:
+    employee_data = json.load(file)
+
+@app.get('/birthdays')
+def birthdays():
+    month = request.args.get('month')
+    department = request.args.get('department')
+
+    filtered_employees = [emp for emp in employee_data if emp['Department'] == department and emp['Birthday'][5:7] == month]
+
+    response = {
+        "total": len(filtered_employees),
+        "employees": filtered_employees
+    }
+
+    return jsonify(response)
+
+@app.get('/anniversaries')
+def anniversaries():
+    month = request.args.get('month')
+    department = request.args.get('department')
+
+    filtered_employees = [emp for emp in employee_data if emp['Department'] == department and emp['Hiring Date'][5:7] == month]
+
+    response = {
+        "total": len(filtered_employees),
+        "employees": filtered_employees
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
